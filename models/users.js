e442d94ec8d17ed47e87fa565a527b3bc8394c71
Robert Powers <bobpowers@me.com>
2018-01-16 18:52:44 -0600

    fix sequelize bug again

--- models/users.js
+++ models/users.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
-  var Users = sequelize.define("users", {
+  var Users = sequelize.define("Users", {
     id: {
       type: DataTypes.UUID,
       primaryKey: true,
