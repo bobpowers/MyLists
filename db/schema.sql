f92c0689ef12a2af737f0ff37cb170b08d0e31a0
Matthew Fallucca <mfallucca@Ashleys-MacBook-Pro.local>
2018-01-16 19:06:21 -0600

    modifying schema to only include db creation

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,3 @@
--- NO LONGER USED --
--- NO LONGER USED --
--- NO LONGER USED --
--- NO LONGER USED --
--- NO LONGER USED --
--- NO LONGER USED --
-
-DROP DATABASE IF EXISTS lists_db;
-CREATE database lists_db;
-USE lists_db;
-
-CREATE table list (
-    item_id INT(11) NOT NULL AUTO_INCREMENT,
-    list_title varchar(40) NOT NULL,
-    listed_item varchar(100) NOT NULL,
-    task_active BOOLEAN DEFAULT TRUE,
-    user_id INT(11) NOT NULL,
-    date TIMESTAMP,
-    PRIMARY KEY (item_id)
-);
-
-CREATE table users (
-	id INT(11) NOT NULL AUTO_INCREMENT,
-	email varchar(50) NOT NULL,
-	PRIMARY KEY (id)
-);
+DROP DATABASE IF EXISTS mylists_db;
+CREATE database mylists_db;
+USE mylists_db;
