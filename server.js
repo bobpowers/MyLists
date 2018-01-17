50d5cc73dbefc721ee2c74ccaebc86aef557ba1a
Matthew Fallucca <mfallucca@Ashleys-MacBook-Pro.local>
2018-01-16 19:54:15 -0600

    modifying the sync

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ var routes = require('./controllers/mylists_controller.js');
 app.use('/', routes);
 
 // Start listening.
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
