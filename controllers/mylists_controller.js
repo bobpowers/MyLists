e6e17ec148a0472c2752cd9ea4aab9b488a4e4d6
unknown <matthew.fallucca@gmail.com>
2018-01-19 21:17:15 -0600

    adding a couple more routes

--- controllers/mylists_controller.js
+++ controllers/mylists_controller.js
@@ -5,37 +5,36 @@ var task = require('../models/index.js');
 
 router.get('/', function(req, res) {
     
-    // Sequelize Query to get all burgers from database (and join them to their devourers, if applicable)
-    task.Lists.findAll({
-        include: [{model: task.Users}]
+    task.Items.findAll({
+        include: [{model: task.Lists}]
        }).then(function(data){
      
-         // Pass the returned data into a Handlebars object and then render it
-         var hbsObject = { Lists: data };
-         // console.log(data);
+         var hbsObject = { Items: data };
          res.render('index', hbsObject);
      
        })
 });
 
-
-
-
-router.post('/', function(req, res) {
-    // var name = req.body.name;
-    // task.post(name, function() {
-    //     res.redirect('/');
-    // });
+router.post('/api/checked/:id', function(req, res) {
+    models.Items.findOne( {where: {id: req.params.id} } )
+    .then(function(checkedItem){
+        checkedItem.update({
+        task_active: false,
+        })
+    .then(function(){
+        res.redirect('/')
+    })
+    });
 });
 
-router.put('/:id', function(req, res) {
-    // var property = req.body.devoured;
-    // var selector = req.params.id;
-
-    // task.update(
-    //     property, selector, function() {
-    //         res.redirect('/');
-    //     });
+router.put('/api/delete/:id', function(req, res) {
+    models.Items.findOne( {where: {id: req.params.id} } )
+    .then(function(deleteItem){
+        deleteItem.destroy()
+    .then(function(){
+            res.redirect('/')
+        })
+    })
 });
 
 module.exports = router;
